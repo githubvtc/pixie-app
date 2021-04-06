adb1c1c9de43c4224e84c72b62c0d0fdd7eaa825
githubvtc <vandychoo@gmail.com>
2021-04-06 14:47:03 -0400

    FIX:pom.xml,testmainapp
    updating junit,and log4j dependancies

--- src/test/java/com/pixie/main/TestMainApp.java
+++ src/test/java/com/pixie/main/TestMainApp.java
@@ -4,33 +4,33 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestMainApp {
 
-	final static Logger LOG = Logger.getLogger(TestMainApp.class);
+	final static Logger LOG = LogManager.getLogger(TestMainApp.class);
 	
 	private MainApp app = null;
 	
 	@Before
 	public void init() throws FileNotFoundException, IOException {
 		app = new MainApp();
-		InputStream inStream = getClass().getResourceAsStream("/pixie-log4j.properties");
+//		InputStream inStream = getClass().getResourceAsStream("/pixie-log4j.properties");
 		
 		// Because properties file is custom name, it needs to be configured.
-		PropertyConfigurator.configure(inStream);
 		
 	}
 	@Test
 	public void testRun() {
+		System.out.println("Check");
 		if (app != null ) {
 			app.runApp();
 			LOG.info("Running log4j... ");
+			LOG.info("just checking");
 			assertTrue(true);
 		} else {
 			assertTrue(false);
